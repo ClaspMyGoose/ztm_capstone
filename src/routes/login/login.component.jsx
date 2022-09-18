4b769bcad255ed9f29e6cfb0146003c993cde6ba
Michael Bearden <michael.bearden1@outlook.com>
2022-09-18 14:12:05 -0500

    more auth setup, +  login component

--- src/routes/login/login.component.jsx
+++ src/routes/login/login.component.jsx
@@ -1,8 +1,19 @@
+import { googleSignIn, createUserDocument } from '../../utils/firebase/firebase.utils';
 
 
-const Login = () => (
-  <div>Sign In Page</div>
-); 
+const Login = () => {
+
+  const logGoogleUser = async () => {
+    const { user } = await googleSignIn();
+    createUserDocument(user);
+  }
+
+  return (
+    <div>
+      <button onClick={logGoogleUser}>Log in with Google</button>
+    </div>
+  )
+}; 
 
 export default Login
 
