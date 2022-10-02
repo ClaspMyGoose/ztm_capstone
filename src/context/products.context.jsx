e1c0243c979b04375a4a8a07afb409ab015bcfb2
Michael Bearden <michael.bearden1@outlook.com>
2022-10-02 10:01:15 -0500

    added all shop items to firebase. working on dynamic pull of store items from db and display onto separate shop pages

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,13 +1,25 @@
-import { useState, createContext } from 'react';
-import SHOP_JSON from '../shop-data.json'; 
+import { useState, createContext, useEffect } from 'react';
+import { getProducts } from '../utils/firebase/firebase.utils';
+
 
 export const ProductContext = createContext({
   products: [] 
 })
 
+
+
+
+
 export const ProductProvider = ({ children }) => {
 
-  const [products, setProducts] = useState(SHOP_JSON);
+
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, [])
+
 
   const value = { products, setProducts };
 
